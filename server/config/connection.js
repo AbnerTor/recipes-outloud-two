0347bfe917f75e1edafc9f083898f3e9ba206431
ABNER TORIBIO <abnertoribio@live.com>
2021-07-10 10:30:17 -0400

    changing server file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/new', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/team-baba', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
