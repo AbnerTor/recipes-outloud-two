de25c187c8517b08d27f68debc6caaed7385fb03
ABNER TORIBIO <abnertoribio@live.com>
2021-07-10 11:34:20 -0400

    deployment attempt #232424234

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/team-baba', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/recipes-outloud', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
