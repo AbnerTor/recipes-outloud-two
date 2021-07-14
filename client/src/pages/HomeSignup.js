2348d830230cb9f1dd8e22e8ce371de04eb1aef7
Broc C <bac5806@gmail.com>
2021-07-14 19:31:10 -0400

    hit api key in .env file

--- client/src/pages/HomeSignup.js
+++ client/src/pages/HomeSignup.js
@@ -63,8 +63,6 @@ const Signup = (props) => {
 
         <Link className="w-full underline ml-8" to="/signin">← Go to Sign in</Link>
 
-        <Link className="w-full underline ml-8" to="/search"> Go to Search</Link>
-
         <h2 className="flex justify-center mr-8">Signup</h2>
 
         <form className="flex flex-col justify-center" onSubmit={handleFormSubmit}>
