3a47e2000ee8f0b6a0272d5f8c39965bd0302210
ABNER TORIBIO <abnertoribio@live.com>
2021-07-14 19:07:04 -0400

    updating header home block

--- client/src/components/HeaderHome.js
+++ client/src/components/HeaderHome.js
@@ -1,2 +1,24 @@
-import
+import React from 'react';
+import { IconContext } from 'react-icons';
+import { GiCookingPot } from 'react-icons/gi';
+import { IoMenuOutline } from 'react-icons/io5';
 
+
+const HeaderHome = () => {
+
+    return (
+        <div className="flex flex-col bg-blue-200 mb-10 pt-24">
+            <Link to='/' className='px-12 flex items-center'>
+                <IconContext.Provider value={{ color: 'green', size: '3.5rem' }}>
+                    <div>
+                        <GiCookingPot />
+                    </div>
+                </IconContext.Provider>
+                <h1 className='pl-4 text-green'>RECIPES OUTLOUD</h1>
+            </Link>
+            <p className="pl-4">Expand Your Joy of Cooking!</p>
+        </div>
+    )
+}
+
+export default HeaderHome;
