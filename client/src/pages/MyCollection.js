b34fa205daf9af1fea66b1e8cd731c35abfb6710
Ghibli <aran.t.ali@gmail.com>
2021-07-14 16:32:57 -0400

    MyCollection is added and renders partially

--- client/src/pages/MyCollection.js
+++ client/src/pages/MyCollection.js
@@ -1,77 +1,104 @@
 
 // NOTE TO TEAM: MYCOLLECTION.JS HAS REMOVE_RECIPE FUNCTIONALITY.
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Redirect, useParams } from 'react-router-dom';
 import { useQuery, useMutation } from '@apollo/client';
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { QUERY_USER, GET_ME } from '../utils/queries';
 import { REMOVE_RECIPE } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 
-import Header from '../components/Header';
-import MyCard from '../components/MyCard';
+import Navbar from '../components/Navbar';
+import MyList from '../components/MyList';
 
 import { removeRecipeId } from '../utils/localStorage';
 
 const MyCollection = () => {
-  
-  const { loading, error, data } = useQuery(QUERY_ME, {
-    fetchPolicy: 'network-only'
+  const { username: userParam } = useParams();
+
+  const { loading, data } = useQuery(userParam ? QUERY_USER : GET_ME, {
+    variables: { username: userParam },
   });
-  const userData = data?.me || [];
-  // userData has the savedRecipes array
-  console.log(userData);
-  
-  // create function that accepts the recipe's mongo _id value as param and deletes the recipe from the database
 
-  const [removeRecipe] = useMutation(REMOVE_RECIPE);
-
-  const handleDeleteRecipe = async (recipeId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
-    try {
-  // removeRecipe is sent to Apollo Server
-    const { data } = await removeRecipe({
-      variables: { recipeId },
-    });
-
-    // upon success, remove recipe's id from localStorage
-      removeRecipeId(recipeId);
-     
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  // if data isn't here yet, say so
-  if (loading) {
-    return <h2>LOADING...</h2>;
+  const user = data?.me || data?.user || {};
+  // redirect to personal profile page if username is yours
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Redirect to="/me" />;
   }
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this. Please use the navigation links above to sign up or sign in!
+      </h4>
+    );
+  }
+
+  
+  // // create function that accepts the recipe's mongo _id value as param and deletes the recipe from the database
+  // const [removeRecipe] = useMutation(REMOVE_RECIPE);
+
+  // const handleDeleteRecipe = async (recipeId) => {
+  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
+  //   if (!token) {
+  //     return false;
+  //   }
+  //   try {
+  // // removeRecipe is sent to Apollo Server
+  //   const { data } = await removeRecipe({
+  //     variables: { recipeId },
+  //   });
+
+  //   // upon success, remove recipe's id from localStorage
+  //     removeRecipeId(recipeId);
+     
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+
+  // // if data isn't here yet, say so
+  // if (loading) {
+  //   return <h2>LOADING...</h2>;
+  // }
+
 // MYCOLLECTION PAGE AND ITS COMPONENTS WILL BE DESIGNED (At the moment it is arbitrarily set to give an idea)
-// new push 
   return (
     <>
       <div>
+      
+        <h2>
+            {user.savedRecipes.length
+              ? `Viewing ${user.savedRecipes.length} saved ${user.savedRecipes.length === 1 ? 'recipe' : 'recipes'}:`
+              : 'You have no saved recipes!'}
+        </h2>
+        <div>
+          <MyList
+            recipes={user.recipes}
+            title={`${user.username}'s collection...`}
+            showTitle={false}
+            showUsername={false}
+          />  
+        </div>
       </div>
-      <h2>
-          {userData.savedRecipes.length
-            ? `Viewing ${userData.savedRecipes.length} saved ${userData.savedRecipes.length === 1 ? 'recipe' : 'recipes'}:`
-            : 'You have no saved recipes!'}
-      </h2>
-      <section id="my-collection">
-        <figure>
-          {userData.savedRecipes.map((recipe) => (
-            <MyCard title={recipe.title} key={recipe.id} featuresA={recipe.featuresA} featuresB={recipe.featuresB} image={recipe.image} link={recipe.link} nutri={recipe.nutri} />
-          ))}
-        </figure>
-      </section>
     </>
   );
 };
+
+
+
+
+// const { loading, error, data } = useQuery(QUERY_ME, {
+//   fetchPolicy: 'network-only'
+// });
+// const userData = data?.me || [];
+// // userData has the savedRecipes array
+// console.log(userData);
+
 
 export default MyCollection;
