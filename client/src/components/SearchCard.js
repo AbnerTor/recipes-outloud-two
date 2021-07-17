3c731778d866945038b011ebbc4651f0832a6b82
Broc C <bac5806@gmail.com>
2021-07-17 12:03:23 -0400

    debugging errors

--- client/src/components/SearchCard.js
+++ client/src/components/SearchCard.js
@@ -1,51 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { SAVE_RECIPE } from '../utils/mutations';
-
-import Auth from '../utils/auth';
-
-import { searchByIngredient } from '../utils/API';
-import { searchRandomRecipe } from '../utils/API';
-
-const SearchCard = () => {
-
-    return (
-        <>
-            <div className="flex flex-col w-1/3 bg-blue-200">
-                <h2>Recipe Search</h2>
-                <form>
-
-                    <div className="flex-row space-between my-2">
-
-                    </div>
-
-                    <div className="flex-row space-between my-2">
-
-                    </div>
-
-                    <div className="flex-row space-between my-2">
-
-                    </div>
-
-                    <div className="flex-row space-between my-2">
-
-                    </div>
-
-                    <div className="flex-row flex-end">
-                        <button className="flex justify-center bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" type="submit">Submit Search</button>
-                    </div>
-                </form>
-            </div>
-        </>
-    )
-}
-
-
 import '../App.css';
 
 function SearchCard(props) {
-
 // Creating the URL link from recipe.title of the complexSearch results 
   const linkBody = props.title.replace(/\s+/g, '-').toLowerCase();
   console.log(linkBody);
