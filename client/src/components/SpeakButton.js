1ce976d283cd4beb93101a32eab1af9fcfbbf829
Brian McMullen <clanmcmullen2012@gmail.com>
2021-07-20 03:34:03 -0400

    added play button to speakButton for visual que

--- client/src/components/SpeakButton.js
+++ client/src/components/SpeakButton.js
@@ -10,9 +10,9 @@ function SpeakButton(props) {
 
         <button
 
-            className='bg-blue-800 rounded text-white w-40 clear-both'
+            className='bg-blue-800 rounded text-white w-40 clear-both justify-center'
             onClick={() => speak(wordsToSpeak)}>
-            Out-Loud
+            Outloud <svg class="align-middle inline h-5 w-5 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" />  <polygon points="10 8 16 12 10 16 10 8" /></svg>
         </button>
 
     )
